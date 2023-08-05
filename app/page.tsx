0b4f3686f70783b9fd5904e52babbef30f0ce2d6
Kostiantyn Nahula <kostyannagula@gmail.com>
2023-08-05 09:19:00 +0300

    Mapped product images and set stylesheets

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const response = await fetch('https://fakestoreapi.com/products')
   const products: Product[] = await response.json();
   return (
-    <main className="">
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 mt-48">
       <section className="flex flex-col space-y-12 pb-44">
         <h1 className="text-5xl font-bold text-center">Deals of the day</h1>
         {products.map((product) => (
--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import ProductImage from '@/components/ProductImage'
 
 type Props = {
   product: Product
@@ -6,13 +7,20 @@ type Props = {
 
 const Product = ({ product }: Props) => {
   return (
-    <Link href={`/product/${product.id}`}>
-      <div>
-
+    <Link 
+      prefetch={false} 
+      href={`/product/${product.id}`}
+      className='h-96 flex flex-col border group-hover:scale-105 transition-transform ease-out duration-200'
+    >
+      <div className='relative max-h-72 flex-1'>
+        <ProductImage product={product} fill/>
       </div>
-      <div>
+      <div className='font-semibold flex items-center justify-between mt-4 mb-1'>
         <p>{product.title}</p>
-        <p>{product.price}</p>
+        <p>${product.price}</p>
+        <p className='italic text-xs w-64 line-clamp-2 text-gray-600'>
+          {product.description}
+        </p>
       </div>
     </Link>
   )
